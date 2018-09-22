f5441e6905e43cfeb0c937fd2e05079fac0eebc8
pergola <lolpsxvipkry2@gmail.com>
2018-09-22 00:00:22 -0300

    comentando não-refatorados e adicionando funções-base de vértices

--- adjlist/graphLista.h
+++ adjlist/graphLista.h
@@ -1,18 +1,17 @@
-struct graph {
-    int edges;
-    int* vertexes;
-    int** matrix;
+struct node {
+    char vertex;
+    struct node* next;
 };
 
-typedef struct graph Graph;
+typedef struct node Graph;
 
 Graph initGraph();
 void insertVertex(Graph *g, char v);
-void insertEdge(Graph *g, char v, char u);
-void removeVertex(Graph *g, char v);
-void removeEdge(Graph *g, char v, char u);
-int isNeighbour(Graph g, char v, char u);
-void neighbours(Graph g, char v);
+//void insertEdge(Graph *g, char v, char u);
+//void removeVertex(Graph *g, char v);
+//void removeEdge(Graph *g, char v, char u);
+//int isNeighbour(Graph g, char v, char u);
+//void neighbours(Graph g, char v);
 int vertexSize(Graph g);
-int edgeSize(Graph g);
-void endGraph(Graph *g);
+//int edgeSize(Graph g);
+//void endGraph(Graph *g);
--- adjlist/main.c
+++ adjlist/main.c
@@ -7,28 +7,28 @@ int main() {
     Graph *g = &graph;
 
     insertVertex(g, 'A');
-    insertVertex(g, 'B');
     insertVertex(g, 'E');
+    insertVertex(g, 'C');
     insertVertex(g, 'D');
 
-    insertEdge(g, 'A', 'B');
-    insertEdge(g, 'A', 'E');
-
-    removeVertex(g, 'A');
-    insertVertex(g, 'A');
-
-    insertEdge(g, 'A', 'B');
-    insertEdge(g, 'A', 'E');
-    insertEdge(g, 'A', 'A');
-
-    printf("A->B: %d\n", isNeighbour(graph, 'A', 'B'));
-    printf("E->B: %d\n", isNeighbour(graph, 'E', 'B'));
-
-    neighbours(graph, 'A');
-
-    printf("Edges: %d\n", edgeSize(graph));
+//    insertEdge(g, 'A', 'B');
+//    insertEdge(g, 'A', 'E');
+//
+//    removeVertex(g, 'A');
+//    insertVertex(g, 'A');
+//
+//    insertEdge(g, 'A', 'B');
+//    insertEdge(g, 'A', 'E');
+//    insertEdge(g, 'A', 'A');
+//
+//    printf("A->B: %d\n", isNeighbour(graph, 'A', 'B'));
+//    printf("E->B: %d\n", isNeighbour(graph, 'E', 'B'));
+//
+//    neighbours(graph, 'A');
+//
+//    printf("Edges: %d\n", edgeSize(graph));
     printf("Vertexes: %d\n", vertexSize(graph));
-
-    endGraph(g);
+//
+//    endGraph(g);
     return 0;
 }
